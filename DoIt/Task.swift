5b6872a7918ffab5966920052dee36ba9b03d2a4
Gaby G <GabyG@Gabriellas-Air.home>
2017-02-04 19:43:06 -0500

    Each cell now displays a different task: has an exclamation point if important

---
+++ DoIt/Task.swift
@@ -0,0 +1,14 @@
+//
+//  Task.swift
+//  DoIt
+//
+//  Created by Gabriella Gonzalez on 2/4/17.
+//  Copyright © 2017 Gaby G. All rights reserved.
+//
+
+import Foundation
+
+class Task {
+    var name = ""
+    var important = false
+}
